5a548678fa0757ef687f1b93724e786a0d73f504
Saidi Qemali <78086708+saidiqemali@users.noreply.github.com>
2021-02-07 17:45:32 +0100

    Add files via upload

---
+++ Erster Java Code.java
@@ -0,0 +1,29 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		String ort = "Bülach";
+        String tag = "Sonntag";
+        String text = "MDMDFSS";
+        String name = "Gigi";
+		
+		
+		System.out.println(ort);
+		System.out.println("Heute ist " +tag);
+		boolean Iamusingalaptop  = true;
+		System.out.println(Iamusingalaptop); 
+		String ich = "Saidi";
+		System.out.println("A: Wie heisst du ? ");
+		System.out.println("B: "+ name);
+		
+		if (ich == "saidi") {
+		  System.out.println("Hello Saidi");
+		} else {
+		  System.out.println("A: Hallo " + name );
+		}
+	
+		
+	}
+
+}
